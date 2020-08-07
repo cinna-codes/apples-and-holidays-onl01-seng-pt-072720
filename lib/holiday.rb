bfbd9bad8da09539b7b1274de6dc480db3b4323d
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-07 18:39:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holidays, supply|
       if supply == "BBQ"
-        puts holidays
+        return holidays
       end
     end
   end
